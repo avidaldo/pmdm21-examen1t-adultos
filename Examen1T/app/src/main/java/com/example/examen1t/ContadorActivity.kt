156465110c88c58871ec2bad39ffefdf0bfa9a69
avidaldo <avd.formacion@gmail.com>
2021-12-04 18:23:49 +0100

    revisa email completo, muestra contraseña sin 2 EditText

--- Examen1T/app/src/main/java/com/example/examen1t/ContadorActivity.kt
+++ Examen1T/app/src/main/java/com/example/examen1t/ContadorActivity.kt
@@ -12,9 +12,16 @@ class ContadorActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityContadorBinding
 
-    var cuenta1 = 0
-    var cuenta2 = 0
-    var cuentaG = 0
+
+    /* Utilizamos los setters de las propiedades para actualizar automáticamente la UI */
+
+    private var cuenta1 = 0
+        set(value) { field=value; binding.tvCuenta1.text = field.toString() }
+    private var cuenta2 = 0
+        set(value) {  field=value; binding.tvCuenta2.text = field.toString() }
+    private var cuentaG = 0
+        set(value) { field=value;  binding.tvCuentaGlobal.text = field.toString() }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,9 @@ class ContadorActivity : AppCompatActivity() {
         binding.button1.setOnClickListener {
             try {
                 val incremento = binding.et1.text.toString().toInt()
-                alerta(binding.tvIncremento1, false)
                 cuenta1 += incremento
                 cuentaG += incremento
-                updateDisplay()
+                alerta(binding.tvIncremento1, false)
             } catch (e: NumberFormatException) {  // Si el EditText de incremento no tiene un número
                 alerta(binding.tvIncremento1, true)
                 Toast.makeText(this, R.string.no_incremento, Toast.LENGTH_SHORT).show()
@@ -37,43 +43,24 @@ class ContadorActivity : AppCompatActivity() {
         binding.button2.setOnClickListener {
             try {
                 val incremento = binding.et2.text.toString().toInt()
-                alerta(binding.tvIncremento2, false)
                 cuenta2 += incremento
                 cuentaG += incremento
-                updateDisplay()
+                alerta(binding.tvIncremento2, false)
             } catch (e: NumberFormatException) {
                 alerta(binding.tvIncremento2, true)
                 Toast.makeText(this, R.string.no_incremento, Toast.LENGTH_SHORT).show()
             }
         }
 
-        binding.ivReset1.setOnClickListener {
-            cuenta1 = 0
-            updateDisplay()
-        }
-
-        binding.ivReset2.setOnClickListener {
-            cuenta2 = 0
-            updateDisplay()
-        }
-
-        binding.ivResetG.setOnClickListener {
-            cuentaG = 0
-            updateDisplay()
-        }
+        binding.ivReset1.setOnClickListener { cuenta1 = 0 }
+        binding.ivReset2.setOnClickListener { cuenta2 = 0 }
+        binding.ivResetG.setOnClickListener { cuentaG = 0 }
 
     }
 
     private fun alerta(tv: TextView, alerta: Boolean) {
         if (alerta) tv.setTextColor(Color.RED)
         else tv.setTextColor(Color.BLACK)
-    }
-
-
-    private fun updateDisplay() {
-        binding.tvCuenta1.text = cuenta1.toString()
-        binding.tvCuenta2.text = cuenta2.toString()
-        binding.tvCuentaGlobal.text = cuentaG.toString()
     }
 
 }
--- Examen1T/app/src/main/java/com/example/examen1t/MainActivity.kt
+++ Examen1T/app/src/main/java/com/example/examen1t/MainActivity.kt
@@ -3,17 +3,33 @@ package com.example.examen1t
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View.INVISIBLE
-import android.view.View.VISIBLE
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.util.Patterns
 import android.widget.Toast
 import com.example.examen1t.databinding.ActivityMainBinding
 import java.lang.RuntimeException
+import java.util.regex.Pattern
+import android.text.method.PasswordTransformationMethod
+
+
+// https://www.baeldung.com/java-email-validation-regex#regular-expression-by-rfc-5322-for-email-validation
+// const val REGEX_EMAIL_STRING_RFC5322 = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$"
 
 class MainActivity : AppCompatActivity() {
+
+    //val EMAIL_ADDRESS = Pattern.compile(REGEX_EMAIL_STRING_RFC5322)
 
     private lateinit var binding: ActivityMainBinding
 
     private var passwordVisible = false
+        set(value) {
+            binding.tvMuestraContrasena.text =
+                if (value) getString(R.string.hidePassword)
+                else getString(R.string.showPassword)
+            //Toast.makeText(this, binding.editTextTextPassword.inputType.toString(), Toast.LENGTH_SHORT).show()
+            field = value
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +46,7 @@ class MainActivity : AppCompatActivity() {
                 checkPassword()
                 startActivity(Intent(this, ContadorActivity::class.java))
             } catch (e: InvalidEmailException) {
-                Toast.makeText(this, R.string.empty_user, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             } catch (e: InvalidPasswordException) {
                 Toast.makeText(this, e.message, Toast.LENGTH_SHORT).show()
             }
@@ -43,39 +59,47 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun changePasswordVisibility() { // TODO: Hacerlo bien
-        if (passwordVisible) {
-            binding.editTextPassMostrado.visibility = INVISIBLE
-            binding.editTextTextPassword.text = binding.editTextPassMostrado.text
-            binding.editTextTextPassword.visibility = VISIBLE
-            binding.tvMuestraContrasena.text = getString(R.string.showPassword)
-            passwordVisible = false
-        } else {
-            binding.editTextTextPassword.visibility = INVISIBLE
-            binding.editTextPassMostrado.text = binding.editTextTextPassword.text
-            binding.editTextPassMostrado.visibility = VISIBLE
-            binding.tvMuestraContrasena.text = getString(R.string.hidePassword)
-            passwordVisible = true
-
-        }
+    private fun changePasswordVisibility() {
+        changePasswordVisibility1()
+        //changePasswordVisibility2()
     }
 
-    class InvalidEmailException : RuntimeException()
+    private fun changePasswordVisibility1() {
+        /* Solución de bajo nivel (sacada de leer el código) */
+        binding.editTextTextPassword.inputType =
+            if (passwordVisible) InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD  // 129
+            else InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD // 145
+        passwordVisible = !passwordVisible
+    }
+
+    private fun changePasswordVisibility2() {
+        /* https://developer.android.com/reference/android/text/method/TransformationMethod */
+        binding.editTextTextPassword.transformationMethod =
+            if (passwordVisible) PasswordTransformationMethod()
+            else HideReturnsTransformationMethod.getInstance() // null
+        passwordVisible = !passwordVisible
+    }
+
+    class InvalidEmailException(message: String) : RuntimeException(message)
     class InvalidPasswordException(message: String) : RuntimeException(message)
 
     private fun checkEmail() {
-        if (binding.editTextTextEmailAddress.text.isEmpty())
-            throw InvalidEmailException()
+        val emailString = binding.editTextTextEmailAddress.text.toString()
+
+        if (emailString.isEmpty())
+            throw InvalidEmailException(getString(R.string.emptyString))
+        //if (!EMAIL_ADDRESS.matcher(emailString).matches())  // Con patrón RFC5322
+        if (!Patterns.EMAIL_ADDRESS.matcher(emailString).matches()) // Con patrón en librería de Android
+            throw InvalidEmailException(getString(R.string.email_dont_match_pattern))
     }
 
 
     private fun checkPassword() {
-        val password =
-            if (passwordVisible) binding.editTextPassMostrado.text.toString()
-            else binding.editTextTextPassword.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
 
         if (password.isEmpty()) throw InvalidPasswordException(getString(R.string.empty_password))
         if (password.length <= 8) throw InvalidPasswordException(getString(R.string.invalid_password))
     }
+
 
 }
